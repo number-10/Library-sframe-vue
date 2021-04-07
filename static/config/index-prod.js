daab4a013cb75d87729d584c7763c2480bf76351
number10 <number_fan@outlook.com>
2021-04-07 17:08:16 +0800

    diz

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -9,7 +9,7 @@
   // window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/renren-fast';
   //window.SITE_CONFIG['baseUrl'] = 'http://www.number10.cn:9001/renren-fast';
 
-  window.SITE_CONFIG['baseUrl'] = 'http://localhost:19001/base';
+  window.SITE_CONFIG['baseUrl'] = 'http://number10.cn:19001/base';
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
   window.SITE_CONFIG['version'] = '';   // 版本号(年月日时分)
